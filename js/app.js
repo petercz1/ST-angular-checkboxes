3a699aa7e0ef6d3ec0b69de022787380f5dbfdd6
petercz1 <email.peter.carroll@gmail.com>
2017-12-15 11:39:58 +0800

    If only Dionysus were alive!  Where would he eat? 		-- Woody Allen

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// app.js
+
+var formApp = angular.module('formApp', [])
+
+    .controller('formController', function($scope) {
+
+        // we will store our form data in this object
+        $scope.formData = {};
+
+    });
